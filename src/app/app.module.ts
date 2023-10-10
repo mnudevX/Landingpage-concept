392020d2c83a769654b796e6af4d6c3081811d8c
MUdevDatec24 <115473037+MUdevDatec24@users.noreply.github.com>
2023-10-10 12:41:13 +0200

    add process component + logic/styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { HeroComponent } from './body/hero/hero.component';
+import { AboutComponent } from './body/about/about.component';
+import { ProcessComponent } from './body/process/process.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeroComponent,
+    AboutComponent,
+    ProcessComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/navbar/navbar.component.ts
@@ -0,0 +1,39 @@
+import { Component, HostListener, OnInit } from '@angular/core';
+import { CloseNavService } from 'src/app/close-nav.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+  constructor(private cnService: CloseNavService) {}
+
+  ngOnInit(): void {
+    //nimmt boolean entgegen
+    this.cnService.openNav.subscribe((open) => {
+      this.classApplied = open;
+    });
+  }
+
+  classApplied = false;
+  toggleClass() {
+    this.classApplied = !this.classApplied;
+  }
+
+  // Navbar Sticky
+  isSticky: boolean = false;
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+    if (scrollPosition >= 50) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
+  }
+}
